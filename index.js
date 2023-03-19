258a3bc1995ab61b7e1c1a2e9065263bde3ec612
+ <+>
2023-03-19 16:23:38 -0400

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400){
+    return "This one is on me!"
+  } else if (ride >=400 && ride < 2000){
+    return "That will be twenty bucks."
+  } else if (ride > 2000 && ride < 2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (ride > 2500)  {
+    return "No can do."
+  
+  }
 }
+console.log(scuberGreetingForFeet(199));
+console.log(scuberGreetingForFeet(1500));
+console.log(scuberGreetingForFeet(2001));
+console.log(scuberGreetingForFeet(2501));
 
-function ternaryCheckCity(){
-  // Write your code here!
+const place = NYC
+function ternaryCheckCity(city){
+  const isDestination = city === "NYC" ? 'Ok, sounds good.' : 'No go.';
+  return isDestination;
 }
+console.log(ternaryCheckCity(NYC));
+console.log(ternaryCheckCity(Pittsburg));
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+const tip = someAmount
+
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you."  
+    default:
+      return "Bye.";
+    
+
+  }
 }
